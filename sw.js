365ba7bb5693934a31417648c367d2887123e722
brunocmaragliano-bit <brunocmaragliano@gmail.com>
2026-07-09 22:40:04 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='japao2026-v40';
+const CORE=['./','index.html','manifest.json','icon-180.png','icon-192.png','icon-512.png'];
+self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(CORE);}));});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){if(k!==CACHE)return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
+self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request).catch(function(){return caches.match('index.html');});}));});
